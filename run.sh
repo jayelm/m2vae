cba33d355cc63f734036b806b9f05cd24e6e64ba
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2019-05-01 02:47:07 -0700

    Update run script

--- run.sh
+++ run.sh
@@ -4,11 +4,13 @@ NUM_WORKERS="4"
 MEM="16G"
 EXCLUDE="jagupard4,jagupard5,jagupard6,jagupard7,jagupard8,jagupard13"
 
-BASE_CMD="python m2vae/train.py --cuda --pin_memory --batch_size 128 --epochs 500 --activation relu"
+EPOCHS="1000"
+
+BASE_CMD="python m2vae/train.py --resume --cuda --pin_memory --batch_size 128 --epochs $EPOCHS --activation relu"
 
 for n_tracks in 1 5; do
-    for annealing_epochs in 50 100; do
-        exp_name="exp/$n_tracks""track_$annealing_epochs""anneal"
+    for annealing_epochs in 100 500; do
+        exp_name="exp/small_$n_tracks""track_$annealing_epochs""anneal"
         mkdir -p "$exp_name"
         cmd="$BASE_CMD --n_tracks $n_tracks --annealing_epochs $annealing_epochs --exp_dir $exp_name"
         nlprun -a py36-muj -g 1 -c "$NUM_WORKERS" -r "$MEM" -x "$EXCLUDE" "$cmd" -o "$exp_name/stdout"
