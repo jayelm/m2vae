be0e94bc7d088e7be5714365036d7769397a58ec
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2019-05-24 00:43:05 -0700

    Better vis functions

--- m2vae/vis.py
+++ m2vae/vis.py
@@ -27,9 +27,26 @@ import io_util
 import wrappers
 
 
+DEFAULT_RATE = 22050
 DOWNBEATS = np.array([0, 48, 96, 144])
 DOWNBEATS_ONEHOT = np.zeros(192, dtype=np.bool_)
 DOWNBEATS_ONEHOT[DOWNBEATS] = 1
+
+
+N2MIDI = {
+    0: 0,  # acoustic grand piano; and set is_drum = True
+    1: 0,  # acoustic grand piano
+    2: 24,  # acoustic guitar (nylon)
+    3: 32,  # acoustic bass
+    4: 48,  # string ensemble 1 (nylon)
+}
+N2NAME = {
+    0: 'drum',
+    1: 'piano',
+    2: 'guitar',
+    3: 'bass',
+    4: 'strings'
+}
 
 
 def interpolate(mu1, mu2, steps=3, method='linear'):
@@ -38,7 +55,10 @@ def interpolate(mu1, mu2, steps=3, method='linear'):
     return linfit(all_steps)
 
 
-def to_track(track_np, is_drum=False, name='piano'):
+def to_track(track_np, n=None, is_drum=False, name='piano', program=0):
+    """
+    If n is given, use the values in N2MIDI/N2NAME and ignore name/program
+    """
     if len(track_np.shape) != 3:
         raise ValueError("Pass in an array of shape [n_bar, n_timesteps, n_pitches]")
     n_bar = track_np.shape[0]
@@ -49,10 +69,43 @@ def to_track(track_np, is_drum=False, name='piano'):
     padding_amt = (128 - n_pitches) // 2
     note_padding = np.zeros((tot_timesteps,  padding_amt), dtype=np.bool)
     track_np_flat = np.concatenate((note_padding, track_np_flat, note_padding), axis=1)
+
+    if n is not None:
+        program = N2MIDI[n]
+        name = N2NAME[n]
+        is_drum = n == 0
     track = ppr.Track(pianoroll=track_np_flat,
-                      is_drum=is_drum, name=name)
+                      is_drum=is_drum, name=name, program=program)
 
     return track
+
+
+def to_multitrack(mt, n=None):
+    """
+    Create a multitrack output out of a model tensor
+    Input is [n_bars, n_timesteps, n_pitches, n_tracks] tensor.
+    If n is given, it's a list of length n_tracks, detailing the LPD-5 number
+    for each track.
+    TODO: Support custom programs/names just like to_track.
+    """
+    n_tracks = mt.shape[3]
+    if n is not None and len(n) != n_tracks:
+        raise ValueError("Must supply n == n_tracks")
+    tracks = []
+    for i in range(n_tracks):
+        if n is None:
+            this_n = i
+        else:
+            this_n = n[i]
+        tracks.append(to_track(mt[:, :, :, i], n=i))
+    return ppr.Multitrack(tracks=tracks, beat_resolution=12,
+                          downbeat=DOWNBEATS_ONEHOT)
+
+
+def synthesize(mt, rate=DEFAULT_RATE):
+    midi = mt.to_pretty_midi()
+    audio = midi.fluidsynth(fs=rate)
+    return audio
 
 
 def plot_track(track):
@@ -60,18 +113,10 @@ def plot_track(track):
     plt.show()
 
 
-def synthesize(track):
-    track_mt = ppr.Multitrack(tracks=[track], beat_resolution=12,
-                              downbeat=DOWNBEATS_ONEHOT)
-    track_midi = track_mt.to_pretty_midi()
-    track_audio = track_midi.fluidsynth()
-    return track_audio
-
-
-def ipy_display(audio, title=None):
+def ipy_display(audio, title=None, rate=DEFAULT_RATE):
     if title is not None:
         display(HTML('<h2>{}</h2>'.format(title)))
-    display(Audio(audio, rate=44100))
+    display(Audio(audio, rate=rate))
 
 
 if __name__ == '__main__':
